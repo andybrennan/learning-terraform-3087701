c19adaa2185d887f73cad7e943beb8ebdb05595a
Andy Brennan <30912456+andybrennan@users.noreply.github.com>
2023-12-01 15:21:56 +0000

    Remove map_public_ip_on_launch

--- main.tf
+++ main.tf
@@ -25,7 +25,6 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -41,7 +40,6 @@ resource "aws_instance" "blog" {
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   associate_public_ip_address = true
-  map_public_ip_on_launch = true
   subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
