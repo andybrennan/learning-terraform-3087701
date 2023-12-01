6277a051d42bc89d7468a76f9ad216a49b6f6d70
Andy Brennan <30912456+andybrennan@users.noreply.github.com>
2023-12-01 14:33:57 +0000

    enable_nat_gateway in right place

--- main.tf
+++ main.tf
@@ -27,6 +27,8 @@ module "blog_vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+  enable_nat_gateway = true
+
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -39,7 +41,6 @@ resource "aws_instance" "blog" {
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  enable_nat_gateway = true
   subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
