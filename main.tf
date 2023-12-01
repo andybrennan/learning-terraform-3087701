1db7a369f31fed0f493cefdbc51863a5478e32a2
Andy Brennan <30912456+andybrennan@users.noreply.github.com>
2023-12-01 14:47:39 +0000

    associate_public_ip_address

--- main.tf
+++ main.tf
@@ -25,9 +25,8 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
@@ -41,6 +40,7 @@ resource "aws_instance" "blog" {
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
+  associate_public_ip_address = true
   subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
