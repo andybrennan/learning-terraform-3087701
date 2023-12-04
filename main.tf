ad84cc26e77f37c66c147cd8c560f7590e648d6b
Andy Brennan <30912456+andybrennan@users.noreply.github.com>
2023-12-04 10:45:31 +0000

    Change name of instance

--- main.tf
+++ main.tf
@@ -36,14 +36,14 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
+  # Needed to set public IP 
   associate_public_ip_address = true
   subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
 
